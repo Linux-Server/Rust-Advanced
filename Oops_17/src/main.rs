af883950db35c886312d68ff0f3702fe03e0d62f
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-10 20:51:53 +0530

    Docs test added

--- Oops_17/src/main.rs
+++ Oops_17/src/main.rs
@@ -1,3 +1,35 @@
+/// The game starts from here a.k.a main()
+
+//Object-Oriented Programming Features of Rust
+
+/*
+ - oops is a paradigm in modelling programs
+ - here we discuss more of benefits and tradeoff of using oops concept in rust
+ - rust is influenced by many paradigm, once of such is oops
+ -  examples of programming paradigm , oops, procedural, functional etc..
+ - The features that rust acquired from oops are objects,encapsulation,inheritance
+ -
+
+ */
+pub mod char_17_1;
+pub mod animal;
+
+// use char_17_1::{encapsulation,inheritance,objects};
 fn main() {
+    //! inner main
+
     println!("Advanced OOPS!");
+
 }
+
+    pub struct Person{
+        // This the name of person
+        pub name:String,
+        /// This is age of the person
+        pub age: i32
+    }
+
+
+
+
+

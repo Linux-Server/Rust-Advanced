cbea9d5c0a133ac431b969d1a3bee447c81d2e2e
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-13 21:55:40 +0530

    workspace setup completed

--- entry_point/src/main.rs
+++ entry_point/src/main.rs
@@ -1,3 +1,15 @@
+#![doc(html_logo_url = "https://rustacean.net/assets/cuddlyferris.svg")]
+//! # Entry Point
+//! This entry point binary crate act as the base crate , which have the ability to call an the other lib crate mentioned in the workspace
+//! ## Examples
+//! ```
+//! fn main() {
+//!     println!("Hello, Entry Point!");
+//! }
+//! ```
+
+use closure::tester;
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Entry Point!");
+    let _x = tester();
 }
